98a607fe89515a2fdef9a97a6916c0fdef38845e
bizwiggle <bizwiggle@gmail.com>
2014-12-07 15:30:52 -0600

    updated for prod server

--- src/painting/stripe_info.py
+++ src/painting/stripe_info.py
@@ -2,6 +2,6 @@ SECRET_KEY = 'sk_test_if262bwhU5FuVhPQ03zCN3NB' #test key
 PUB_KEY = 'pk_test_2YMBTYVt4UNTEBKh5tjMi55e' #test pub key
 
 #SECRET_KEY = 'sk_live_lQxug0CXt7jnQ96uDvPxS6T8' #prod key
-#PUB_KEY = 'pk_live_gbnLr6Jt2ZTMSLLYnX5BZzPe'
+#PUB_KEY = 'pk_live_gbnLr6Jt2ZTMSLLYnX5BZzPe' #prod pub key
 
 
