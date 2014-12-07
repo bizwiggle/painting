f8ea2b20378c5b311859eb62b6199405b1e25c27
bizwiggle <bizwiggle@gmail.com>
2014-12-07 15:32:03 -0600

    added for prod

--- src/painting/base_settings.py
+++ src/painting/base_settings.py
@@ -64,7 +64,7 @@ else:
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'painting',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '4PolarBear$',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         }
