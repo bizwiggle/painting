3bf902a22fd57ac57c4f6adf8005ee791927353b
raymond <raymond@bizwiggle.com>
2014-12-11 12:44:47 -0600

    fix paintings4

--- src/painting/site_settings/painting4_bizwiggle.py
+++ src/painting/site_settings/painting4_bizwiggle.py
@@ -4,7 +4,7 @@ from painting.constants import SITE_IDS
 SITE_NAME = 'painting4.bizwiggle.com'
 SITE_ID = SITE_IDS[SITE_NAME]
 
-STRIPE_PLAN = 'paintingfreee'
+STRIPE_PLAN = 'paintingfree'
 TRIAL_LENGTH = 0
 
 ALLOWED_HOSTS = ['.bizwiggle.com']
