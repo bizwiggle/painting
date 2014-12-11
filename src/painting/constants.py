046669e8235d3d22e81805afcfff0abf1ff5f626
bizwiggle <bizwiggle@gmail.com>
2014-12-11 17:57:21 -0600

    Added ColorCoatPainting

--- src/painting/constants.py
+++ src/painting/constants.py
@@ -18,6 +18,7 @@ SITE_IDS = {
     'painting13.bizwiggle.com':17,
     'painting14.bizwiggle.com':18,
     'painting15.bizwiggle.com':19,
+    'colorcoatpainting.bizwiggle.com':20,
 }
 
 BIZWIGGLE_INFO = {
---
+++ src/painting/site_settings/colorcoatpainting_bizwiggle.py
@@ -0,0 +1,10 @@
+from painting.base_settings import *
+from painting.constants import SITE_IDS
+
+SITE_NAME = 'colorcoatpainting.bizwiggle.com'
+SITE_ID = SITE_IDS[SITE_NAME]
+
+STRIPE_PLAN = 'painting5000NT'
+TRIAL_LENGTH = 0
+
+ALLOWED_HOSTS = ['.bizwiggle.com']
