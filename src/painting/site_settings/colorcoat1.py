ecf4ddb177296aec466ee442dc56eb71b68adddd
bizwiggle <bizwiggle@gmail.com>
2014-12-12 10:20:40 -0600

    added colorcoat1.com

--- src/painting/site_settings/colorcoat1.py
+++ src/painting/site_settings/colorcoat1.py
@@ -1,10 +1,10 @@
 from painting.base_settings import *
 from painting.constants import SITE_IDS
 
-SITE_NAME = 'colorcoatpainting.bizwiggle.com'
+SITE_NAME = 'colorcoat1.com'
 SITE_ID = SITE_IDS[SITE_NAME]
 
 STRIPE_PLAN = 'painting5000NT'
 TRIAL_LENGTH = 0
 
-ALLOWED_HOSTS = ['.bizwiggle.com']
+ALLOWED_HOSTS = ['colorcoat1.com', 'www.colorcoat1.com']
